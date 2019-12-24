b80425eefb54927940806150e1822d7215a1fd62
David Pojunas <davidapojunas@gmail.com>
2019-12-24 14:22:29 +0100

    Dp:

--- youtube_predict.py
+++ youtube_predict.py
@@ -9,29 +9,33 @@ from sklearn.metrics import accuracy_score
 
 
 
-def main():
-    df = pd.read_csv("youtube-new/USvideos.csv", escapechar="\\",skipinitialspace= True)
+# def main():
+#     df = pd.read_csv("youtube-new/USvideos.csv", escapechar="\\",skipinitialspace= True)
     
-    s_plot = pd.DataFrame(columns=['X_data', 'Y_data'])
+#     s_plot = pd.DataFrame(columns=['X_data', 'Y_data'])
 
-    #PREPROCESSING
-    df[["val1", "val2", "val3"]] = df["publish_time"].str.split(pat = "-", expand=True)
-    df[["time1", "time2", "time3"]] = df["trending_date"].str.split(pat = ".", expand=True)
-    df["days_to_trend"] = df["time2"].astype('int32') - df["val2"].astype('int32')
-    df = df.drop(columns=["val1", "val2", "val3", "time1", "time2", "time3"])
+#     #PREPROCESSING
+#     df[["val1", "val2", "val3"]] = df["publish_time"].str.split(pat = "-", expand=True)
+#     df[["time1", "time2", "time3"]] = df["trending_date"].str.split(pat = ".", expand=True)
+#     df["days_to_trend"] = df["time2"].astype('int32') - df["val2"].astype('int32')
+#     df = df.drop(columns=["val1", "val2", "val3", "time1", "time2", "time3"])
     
-    column_name = df.columns[7:11]
-    train_attr, test_attr, train_tar, test_tar = train_test_split(df.loc[0:,column_name],df["days_to_trend"],test_size=0.5)
-    clf = tree.DecisionTreeClassifier()
+#     column_name = df.columns[7:11]
+#     train_attr, test_attr, train_tar, test_tar = train_test_split(df.loc[0:,column_name],df["days_to_trend"],test_size=0.5)
+#     clf = tree.DecisionTreeClassifier()
     
-    # train_clf = 
-    # pred_clf = train_clf.predict(test_attr)
-    # acc_score = accuracy_score(test_tar, pred_clf)
+#     # train_clf = 
+#     # pred_clf = train_clf.predict(test_attr)
+#     # acc_score = accuracy_score(test_tar, pred_clf)
     
-    tree.plot_tree(clf.fit(train_attr, train_tar))
-    dot_data = tree.export_graphviz(clf, out_file=None) 
-    graph = graphviz.Source(dot_data) 
-    graph.render("youtube_data") 
+#     tree.plot_tree(clf.fit(train_attr, train_tar))
+#     dot_data = tree.export_graphviz(clf, out_file=None) 
+#     graph = graphviz.Source(dot_data) 
+#     graph.render("youtube_data") 
+
+def main():
+    us_videos = pd.read_csv("youtube-new/USvideos.csv", escapechar="\\",skipinitialspace= True)
+    us_videos_categories = pd.read_json('../input/US_category_id.json')
     
     
 
